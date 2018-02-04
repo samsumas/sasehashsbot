18b15c777fa3e061d8eee763b38acaa3360cda62
sasehash <s8sashal@stud.uni-saarland.de>
2018-02-04 21:36:56 +0100

    10sec to reset should be ok

--- clean.js
+++ clean.js
@@ -1,9 +1,9 @@
-// ignores everything for 30 seconds
+// ignores everything for 10 seconds
 const Telegraf = require('telegraf');
 require('dotenv').config(); // for apikeys
 
 const bot = new Telegraf(process.env.APIKEY_TELEGRAM);
-setTimeout(() => { throw "Restart" }, 30000);
+setTimeout(() => { throw "Restart" }, 10000);
 bot.hears(/^.*$/, ({ reply }) => reply(`Please standby, i am currently rebooting...`));
 
 bot.startPolling();
