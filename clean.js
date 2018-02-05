d9bdf03043f738aed9e07166e2f8f256c388c785
sasehash <s8sashal@stud.uni-saarland.de>
2018-02-05 16:39:53 +0100

    removed message

--- clean.js
+++ clean.js
@@ -4,6 +4,5 @@ require('dotenv').config(); // for apikeys
 
 const bot = new Telegraf(process.env.APIKEY_TELEGRAM);
 setTimeout(() => { throw "Restart" }, 10000);
-bot.hears(/^.*$/, ({ reply }) => reply(`Please standby, i am currently rebooting...`));
 
 bot.startPolling();
