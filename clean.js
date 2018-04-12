0f97269a23ac2fa27de08578a6efcaf06afff431
sasehash <s8sashal@stud.uni-saarland.de>
2018-04-12 21:51:05 +0200

    abc

--- clean.js
+++ clean.js
@@ -3,6 +3,6 @@ const Telegraf = require('telegraf');
 require('dotenv').config(); // for apikeys
 
 const bot = new Telegraf(process.env.APIKEY_TELEGRAM);
-setTimeout(() => { throw "Restart" }, 10000);
+setTimeout(() => { throw "Restart" }, 1000);
 
 bot.startPolling();
